79df739a28848c783aebc4a35596f6b4d1876daa
ElizCodeSurgeon <andrewselizabeth303@gmail.com>
2025-06-12 22:09:06 +0100

    Updated CSS

--- main.js
+++ main.js
@@ -221,3 +221,15 @@ document.addEventListener("DOMContentLoaded", () => {
     navLinks.classList.toggle("active");
   });
 });
+
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const menuToggle = document.getElementById("menu-toggle");
+  const navLinks = document.getElementById("nav-links");
+
+  menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+  });
+});
